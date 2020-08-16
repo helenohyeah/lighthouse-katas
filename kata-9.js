bcf192d762d3d18aead95e5e16637c0dd0d111fa
helenohyeah <ouyang.helen@gmail.com>
2020-08-16 23:05:26 +0000

    Completed Kata 9 bouncy castle

---
+++ kata-9.js
@@ -0,0 +1,57 @@
+// Use the value below whenever you need the value of Pi
+const PI = 3.14159;
+
+const sphereVolume = function (radius) {
+  return (4 / 3) * PI * Math.pow(radius, 3);
+}
+
+const coneVolume = function (radius, height) {
+  return (1 / 3) * PI * Math.pow(radius, 2) * height;
+}
+
+const prismVolume = function (height, width, depth) {
+  return height * width * depth;
+}
+
+const totalVolume = function (solids) {
+  let volume = 0;
+  for (solid of solids) {
+    if (solid.type === "sphere") {
+      volume += sphereVolume(solid.radius);
+    } else if (solid.type === "cone") {
+      volume += coneVolume(solid.radius, solid.height);
+    } else if (solid.type === "prism") {
+      volume += prismVolume(solid.height, solid.width, solid.depth);
+    }
+  }
+  return volume;
+}
+
+// testing
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); // true
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); // true
+console.log(prismVolume(3, 4, 5) === 60); // true
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000); // true
