9c58ab170421985506d90183766b2994d18c122b
helenohyeah <ouyang.helen@gmail.com>
2020-08-16 21:15:07 +0000

    Completed Kata 6 Stretch repeating numbers

---
+++ kata-6-stretch.js
@@ -0,0 +1,16 @@
+const repeatNumbers = function(data) {
+  let repeated = [];
+  for (let i = 0; i < data.length; i++) {
+    let temp = "";
+    for (let repeat = 0; repeat < data[i][1]; repeat++) {
+      temp += data[i][0];
+    }
+    repeated.push(temp);
+  }
+  return repeated;
+};
+
+// testing
+console.log(repeatNumbers([[1, 10]])); // 1111111111
+console.log(repeatNumbers([[1, 2], [2, 3]])); // 11, 222
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]])); // 10101010, 343434343434, 9292
