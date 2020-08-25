85c092a13820e6ef70043d0a81f8e660d91d22da
helenohyeah <ouyang.helen@gmail.com>
2020-08-25 21:49:10 +0000

    Init commit for Kata 18 number guesser

---
+++ kata-18/kata-18.js
@@ -0,0 +1,15 @@
+// write a guessing game where the user has to guess a secret number
+// after every guess the program tells the user whether the number was too small or too large
+// at the end, print the number of tries
+// inputting the same number multiple times counts as 1 try
+// if user input isn't a number, print an error message and do not count as a try
+
+let prompt = require("prompt-sync")();
+
+// code below (replace this example)
+let answer = prompt("Guess a number: ");
+console.log("You answered: " + answer);
+
+const numberGuesser = function (input) {
+
+}
